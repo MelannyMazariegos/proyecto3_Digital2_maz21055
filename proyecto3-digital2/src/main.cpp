8c27a3f6063929a252f1e1722a8025be1ccf0ee8
MelannyMazariegos <maz21055@uvg.edu.gt>
2023-11-14 04:26:25 -0600

    Update main.cpp

--- proyecto3-digital2/src/main.cpp
+++ proyecto3-digital2/src/main.cpp
@@ -1,8 +1,11 @@
+//Se incluyen librerias
 #include <Arduino.h>
 #include <Wire.h>
 #include<Adafruit_NeoPixel.h>
+//Direccion del sensor para la comunicacion I2C
 #define LM75_ADDRESS 0x48
 Adafruit_NeoPixel tira = Adafruit_NeoPixel(8, 15, NEO_GRB + NEO_KHZ800);
+//Prototipos de funciones
 void calcular(void);
 void medicion(void);
 void tarjeta(void);
@@ -11,6 +14,7 @@ unsigned long tiempoPrevio = 0;  // Almacena el tiempo del último evento
 const int intervalo = 10000;      // Intervalo de tiempo en milisegundos (10 segundos)
 int com=0;
 void setup() {
+  //Se inicializan los monitores seriales
   Serial.begin(115200);
   Serial2.begin(115200);
   tira.begin();
@@ -19,29 +23,32 @@ void setup() {
 }
 
 void loop() {
-  tira.setBrightness(50);
-  tira.setPixelColor(0, 200, 0, 100);
+  tira.setBrightness(50); //Brillo del neopixel
+  tira.setPixelColor(0, 200, 0, 100); //Color del primer pixel
   tira.show();
   //Obtener el tiempo actual
   unsigned long tiempoActual = millis();
   calcular();
   // Verificar si ha pasado el intervalo de tiempo
   if (tiempoActual - tiempoPrevio >= intervalo) {
-    Serial2.println(temperatura);
+    Serial2.println(temperatura); //Se envia la medicion
     // Actualizar el tiempo del último evento
     tiempoPrevio = tiempoActual;
   }
   while (Serial2.available()){
+    //Se lee el caracter que manda el tiva dependiendo de la accion que se este realizando
     char com = Serial2.read();
-    String comando = String(com);
+    String comando = String(com); //Se convierte en caracter en texto
     if (comando == "1"){
+      //Funcion para el patron de neopixel cuando se manda una medicion
       medicion();
     }
     else if (comando == "2"){
+      //Funcion para el patron de neopixel cuando se guarda una medicion
       tarjeta();
     }
     String mensaje = Serial2.readStringUntil('\n');
-    Serial.println(mensaje); //Se imprime el dato en el monitor serial
+    Serial.println(mensaje); //Se imprime la medicion en el monitor serial
   }
   
 }
@@ -49,7 +56,7 @@ void loop() {
 void calcular(){
   // Solicitar temperatura al LM75
   Wire.beginTransmission(LM75_ADDRESS);
-  Wire.write(0x00); // Registro de temperatura (registro 0)
+  Wire.write(0x00); // Registro de temperatura
   Wire.endTransmission();
   
   delay(500); // Esperar para dar tiempo al LM75 para que realice la conversión
@@ -59,7 +66,7 @@ void calcular(){
   if (Wire.available() >= 2) {
     int msb = Wire.read();
     int lsb = Wire.read();
-
+    //Se calcula la temperatura en grados centigrados
     int rawTemperature = ((msb << 8) | lsb);
     temperatura = (rawTemperature * 0.5) / 128.0;
   }
@@ -83,5 +90,3 @@ void tarjeta(){
     tira.show();
   }
 }
-
-
